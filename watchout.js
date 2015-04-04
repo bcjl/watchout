8c80c864f9b5ed285a513656ee7264c7b20b617a
bcjl <briancjliu@gmail.com>
2015-04-04 11:25:13 -0700

    Implemented collision detection for endpoints; scoring also implemented.

--- watchout.js
+++ watchout.js
@@ -3,7 +3,7 @@
 var gameOptions = {
   height: 450,
   width: 700,
-  nEnemies:3,
+  nEnemies:1,
   padding:20
 };
 
@@ -21,7 +21,15 @@ var axes = {
 
 var d3_highScore = d3.select('body').select('.scoreboard').select('.high').select('span');
 var d3_currentScore = d3.select('body').select('.scoreboard').select('.current').select('span');
-var d3_collisionCount = d3.select('body').select('.scoreboard').select('.current').select('span');
+var d3_collisionCount = d3.select('body').select('.scoreboard').select('.collisions').select('span');
+
+var updateScore = function(){
+  gameStats.bestScore = Math.max(gameStats.score, gameStats.bestScore);
+  d3_highScore.data([gameStats.bestScore]).text(function(d){ return d; } );
+  d3_currentScore.data([gameStats.score]).text(function(d){ return d; } );
+  d3_collisionCount.data([gameStats.collisionCount]).text(function(d){ return d; } );
+  // d3_highScore.text()
+};
 
 var d3_gameBoard = d3.select('body').append('div')
 .attr('class', 'game')
@@ -38,14 +46,16 @@ var createEnemies = function(){ // returns an array of objects that contain enem
     return {
       id: elem,
       x: Math.random() * 100,
-      y: Math.random() * 100
+      y: Math.random() * 100,
+      r: 25
     };
   });
 };
 
 var player = [{
   x: 50,
-  y: 50
+  y: 50,
+  r: 50
 }];
 
 var initialize = function(enemyData){
@@ -66,7 +76,7 @@ var initialize = function(enemyData){
     .attr('class', 'enemy')
     .attr('cx', function(enemy){ return axes.x(enemy.x)} )
     .attr('cy', function(enemy){ return axes.y(enemy.y)} )
-    .attr('r', '10px');
+    .attr('r', function(enemy){ return  (enemy.r) + 'px' } );
 
   var d3_player = d3_gameBoard.selectAll('circle.player')
     .data(player);
@@ -76,7 +86,7 @@ var initialize = function(enemyData){
     .attr('class', 'player')
     .attr('cx', function(player){ return axes.x(player.x)} )
     .attr('cy', function(player){ return axes.y(player.y)} )
-    .attr('r', '10px')
+    .attr('r', function(player){ return player.r + 'px'} )
     .call(drag)
     .style({'fill': 'red'});
 
@@ -91,10 +101,29 @@ var render = function(enemyData){
   d3_enemies.attr('class', 'enemy').transition().duration(750)
     .attr('cx', function(enemy){ return axes.x(enemy.x)} )
     .attr('cy', function(enemy){ return axes.y(enemy.y)} )
-    .attr('r', '10px');
+    .attr('r', function(enemy){ return enemy.r + 'px'});
 
 
 };
+
+var checkCollision = function(enemy){
+  var radiusSum = enemy.r + player[0].r;
+  var xDiff = Math.abs(axes.x(enemy.x - player[0].x));
+  var yDiff = Math.abs(axes.y(enemy.y - player[0].y));
+
+  var pythagoreanDistance = Math.sqrt(Math.pow(xDiff,2) + Math.pow(yDiff,2));
+  // console.log(pythagoreanDistance, radiusSum);
+  console.log(xDiff, yDiff);
+  if(pythagoreanDistance < radiusSum){
+    // updateHighScore();
+    gameStats.score = 0;
+    gameStats.collisionCount++;
+    updateScore();
+    console.log('collision detected');
+  }
+};
+
+// game starting below
 
 var enemyArray = createEnemies();
 initialize(enemyArray);
@@ -105,9 +134,12 @@ var update = function(){
     //   elem.x = Math.random(0)*100;
     //   elem.y = Math.random(0)*100;
     // });
+    gameStats.score++;
+    updateScore();
     for(var key in enemyArray){
       enemyArray[key].x = Math.random() * 100;
       enemyArray[key].y = Math.random() * 100;
+      checkCollision(enemyArray[key]);
     }
 
     render(enemyArray);
